ed531087cb1cf9bcaf8879e4087dfe1bdd65c0f3
Joe Tsay <josephtsay@gmail.com>
2022-04-23 09:37:08 -0700

    Update entries for contest 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6e9d53e0f779f086a826f7aa138275d1",
-"index.html": "80d614254d8522bc868fc44c56723412",
-"/": "80d614254d8522bc868fc44c56723412",
-"main.dart.js": "3ce73d95e9986c0685ce568bc4d0b1df",
+"index.html": "fbb2b57627fb908e1bd701777bba4874",
+"/": "fbb2b57627fb908e1bd701777bba4874",
+"main.dart.js": "364ceaff9536a108147ed75526d27a6a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
